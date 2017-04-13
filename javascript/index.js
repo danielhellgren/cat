90a70bf9b8f21bcf8a83d402c6393fdf698655d9
Octoberapples <linneamd@gmail.com>
2017-04-13 07:49:42 +0200

    ändrade färg på navbar

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,18 @@
 
+
+/* HEADER CHANGES SIZE WHEN SCOLLING */
+function init() {
+    window.addEventListener('scroll', function(e){
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 300,
+            header = document.querySelector("header");
+        if (distanceY > shrinkOn) {
+            classie.add(header,"smaller");
+        } else {
+            if (classie.has(header,"smaller")) {
+                classie.remove(header,"smaller");
+            }
+        }
+    });
+}
+window.onload = init();
