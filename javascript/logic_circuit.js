7cc7de521d4428e290af3f244e55b654b7253141
douglasfrol <douglas.froling@hotmail.com>
2017-04-15 15:32:24 +0200

    Fortsatt test av GoJS (AND-grindar & NOT-grindar)

--- javascript/logic_circuit.js
+++ javascript/logic_circuit.js
@@ -20,7 +20,7 @@ function init() {
                 "undoManager.isEnabled": true, // Used for undo/redo
                 allowDrop: true // Allowing nodes to be dropped on the diagram from menu
             });
-
+    /*
     circuit.nodeTemplate =
         MAKE(go.Node, "Auto",
             MAKE(go.Shape,
@@ -31,20 +31,95 @@ function init() {
                 {margin: 3},
                 new go.Binding("text", "key"))
             );
+    */
 
     var circuitModel = MAKE(go.Model); // The Model hold all data, that is, an array of JavaScript objects
     // In th Model, each node is represented by a JavaScript object
     circuitModel.nodeDataArray = [
-        { key: "AND", color: "red" },
-        { key: "OR", color: "yellow" },
-        { key: "NOT", color: "green" }
+        { key: "and", category: "andgate"},
+        { key: "not", category: "notgate"}
     ];
+
     circuit.model = circuitModel;
 
     // Provide custom Node appearance
     // Used for specifying logical gates
-    /*
-    var ANDgate =
-        MAKE(go.Node, "Spot");
-    */
+
+    var andGate =
+        MAKE(go.Node, "Spot",
+            {shadowBlur: 10, shadowColor: "black"},
+            MAKE(go.Shape, "AndGate",
+                {fill: "lightgray", stroke: "black", strokeWidth: 3}
+            ),
+            MAKE(go.Shape, "Rectangle",
+                {fill: "black",
+                 desiredSize: new go.Size(8,8),
+                 fromSpot: go.Spot.Right,
+                 toSpot: go.Spot.Left,
+                 portId: "Input",
+                 alignment: new go.Spot(0, 0.25)
+                }
+            ),
+            MAKE(go.Shape, "Rectangle",
+                {   fill: "black",
+                    desiredSize: new go.Size(8,8),
+                    fromSpot: go.Spot.Right,
+                    toSpot: go.Spot.Left,
+                    portId: "Input",
+                    alignment: new go.Spot(0, 0.75)
+                }
+            ),
+            MAKE(go.Shape, "Rectangle",
+                {   fill: "black",
+                    desiredSize: new go.Size(8,8),
+                    fromSpot: go.Spot.Right,
+                    toSpot: go.Spot.Left,
+                    portId: "Input",
+                    alignment: new go.Spot(1, 0.5)
+                }
+            )
+        );
+
+    var notGate =
+        MAKE(go.Node, "Spot",
+            {shadowBlur: 10, shadowColor: "black"},
+            MAKE(go.Shape, "Inverter",
+                {fill: "lightgray", stroke: "black", strokeWidth: 3}
+            ),
+            MAKE(go.Shape, "Rectangle",
+                {fill: "black",
+                    desiredSize: new go.Size(8,8),
+                    fromSpot: go.Spot.Right,
+                    toSpot: go.Spot.Left,
+                    portId: "Input",
+                    alignment: new go.Spot(0, 0.25)
+                }
+            ),
+            MAKE(go.Shape, "Rectangle",
+                {   fill: "black",
+                    desiredSize: new go.Size(8,8),
+                    fromSpot: go.Spot.Right,
+                    toSpot: go.Spot.Left,
+                    portId: "Input",
+                    alignment: new go.Spot(0, 0.75)
+                }
+            ),
+            MAKE(go.Shape, "Rectangle",
+                {   fill: "black",
+                    desiredSize: new go.Size(8,8),
+                    fromSpot: go.Spot.Right,
+                    toSpot: go.Spot.Left,
+                    portId: "Input",
+                    alignment: new go.Spot(1, 0.5)
+                }
+            )
+        );
+
+    var logicMap = new go.Map("string", go.Node);
+
+    logicMap.add("andgate", andGate);
+    logicMap.add("notgate", notGate);
+    logicMap.add("", circuit.nodeTemplate);
+
+    circuit.nodeTemplateMap = logicMap;
 }
