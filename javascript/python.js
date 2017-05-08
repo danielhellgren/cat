88ab05a803817a6d001ca26266c92c4bd1b02bbf
Octoberapples <linneamd@gmail.com>
2017-05-08 19:51:25 +0200

    Continued on python

--- javascript/python.js
+++ javascript/python.js
@@ -1,5 +1,14 @@
 
 
-$('.code').click( function () {
-    $('.code')
-});
+$().ready(
+    function(){
+        $('.result-btn').click(function () {
+            $('.result-btn').css("background-color","#555555");
+            $('.result-btn').css("color","white");
+            $('.code2').toggle('slow', function() {
+                $('.code2').css("display","block");
+            });
+
+        });
+    }
+);
