56ee1562cb1f6fa8773f7f90552ed5182e041b69
Octoberapples <linneamd@gmail.com>
2017-05-09 11:02:39 +0200

    Continued on python

--- javascript/python.js
+++ javascript/python.js
@@ -1,10 +1,12 @@
 
 
+
+/* FOR DISPLAYING THE CODE */
 $().ready(
     function(){
-        $('.result-btn').click(function () {
-            $('.result-btn').css("background-color","#555555");
-            $('.result-btn').css("color","white");
+        $('#result-btn0').one("click", function () {
+            $('#result-btn0').css("background-color","#555555");
+            $('#result-btn0').css("color","white");
             $('.code2').toggle('slow', function() {
                 $('.code2').css("display","block");
             });
@@ -12,3 +14,38 @@ $().ready(
         });
     }
 );
+
+/* CHECKING THE INPUT */
+$().ready(
+    function() {
+        $('#result-btn1').click(function () {
+            $('#result-btn1').css("background-color", "#555555");
+            $('#result-btn1').css("color", "white");
+            var input = $("input[name=writtenCode]").val();
+            if (input == 'print("Good bye!")') {
+                    $('.code3').css("display", "none");
+                    $('.code4').css("display", "none");
+
+                $('.code3').toggle('slow', function () {
+                    $('.code3').css("display", "block");
+                });
+            }
+            else {
+                $('.code3').css("display", "none");
+                    $('.code4').css("display", "none");
+                $('.code4').toggle('slow', function () {
+                    $('.code4').css("display", "block");
+                });
+            }
+        });
+    });
+
+
+
+/*SO YOU CANT PRESS ENTER ON THE FORM */
+$(document).on('keyup keypress', 'form input[type="text"]', function(e) {
+    if(e.which == 13) {
+        e.preventDefault();
+        return false;
+    }
+});
