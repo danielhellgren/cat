a485e886439a776ca8e41a9393bb8d7e223f9256
danielhellgren <daniel__Hellgren@hotmail.com>
2017-05-17 16:33:34 +0200

    Binary animation upload

--- javascript/binary.js
+++ javascript/binary.js
@@ -1,3 +1,19 @@
-/**
- * Created by Daniel on 2017-04-20.
- */
+
+
+$(document).ready( function() {
+    printBinaryResult();
+});
+
+// count from 0 to 255 and print - loop indefinetly
+function printBinaryResult(){
+
+    var textDiv = document.getElementsByClassName("result-text")[0];
+    var i = 1;
+    window.setInterval(function(){
+        if(i > 255){
+            i = 0;
+        }
+        textDiv.innerHTML = "" + i;
+        i++;
+    }, 500); //repeat every 1 sec
+}
