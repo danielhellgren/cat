eabb9bf265e9d815560e9430c5addb01278ae0a9
Douglas <douglas.froling@hotmail.com>
2017-05-20 18:19:17 +0200

    Mobile menu for all pages

--- javascript/binary.js
+++ javascript/binary.js
@@ -2,6 +2,21 @@
 
 $(document).ready( function() {
     printBinaryResult();
+    document.getElementById("mobile-menu").addEventListener("click", function() {
+        this.classList.toggle("change");
+        var dropdown = document.getElementById("dropdownbinary");
+        dropdown.classList.toggle("show");
+        if (dropdown.classList.contains("show")) {
+            console.log(document.getElementsByClassName("content")[0]);
+            document.getElementsByClassName("content")[0].style.zIndex = "-1";
+            document.getElementById("content-cover-binary").style.display = "inline-block";
+            document.getElementById("content-cover-binary").style.zIndex = "-1";
+        }
+        else {
+            document.getElementsByClassName("content")[0].style.zIndex = "";
+            document.getElementById("content-cover-binary").style.display = "none";
+        }
+    });
 });
 
 // count from 0 to 255 and print - loop indefinetly
--- javascript/python.js
+++ javascript/python.js
@@ -1,4 +1,20 @@
-
+$().ready(function() {
+    document.getElementById("mobile-menu").addEventListener("click", function() {
+        this.classList.toggle("change");
+        var dropdown = document.getElementById("dropdownpython");
+        dropdown.classList.toggle("show");
+        if (dropdown.classList.contains("show")) {
+            console.log(document.getElementsByClassName("content")[0]);
+            document.getElementsByClassName("content")[0].style.zIndex = "-1";
+            document.getElementById("content-cover-python").style.display = "inline-block";
+            document.getElementById("content-cover-python").style.zIndex = "-1";
+        }
+        else {
+            document.getElementsByClassName("content")[0].style.zIndex = "";
+            document.getElementById("content-cover-python").style.display = "none";
+        }
+    });
+});
 
 
 /* FOR DISPLAYING THE CODE */
